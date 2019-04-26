10ce50a6e0852db217139cfb613ca648b1b04ed9
chen <mrchenxk@outlook.com>
2019-04-26 14:10:50 +0800

    11

--- src/main/java/com/clt/ess/base/EssInterceptor.java
+++ src/main/java/com/clt/ess/base/EssInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 public class EssInterceptor implements HandlerInterceptor {
     /**
@@ -28,19 +29,7 @@ public class EssInterceptor implements HandlerInterceptor {
      * Handler执行之前调用这个方法
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-                             Object handler) throws Exception {
-        //获取请求的URL
-        String url = request.getRequestURI();
-        //URL:login.jsp是公开的;这个demo是除了login.jsp是可以公开访问的，其它的URL都进行拦截控制
-        if(url.contains("index")){
-            return true;
-        }
-        if(url.contains(".do")){
-            return true;
-        }
-        if(url.contains(".error")){
-            return true;
-        }
+                             Object handler) throws IOException {
         //获取Session
         HttpSession session = request.getSession();
         User user = (User)session.getAttribute("user");
@@ -48,8 +37,20 @@ public class EssInterceptor implements HandlerInterceptor {
         if(user != null){
             return true;
         }
-        //不符合条件的，跳转到登录界面
-        request.getRequestDispatcher("http://10.40.4.7:8080/EssSealPlatform/unit/logout").forward(request, response);
+        //获取请求的URL
+        String url = request.getRequestURI();
+        //URL:login.jsp是公开的;这个demo是除了login.jsp是可以公开访问的，其它的URL都进行拦截控制
+        if(url.contains("index")){
+            return true;
+        }
+        if(url.contains("do")){
+            return true;
+        }
+        if(url.contains("error")){
+            return true;
+        }
+//        http://10.40.4.7:8080/EssSealPlatform/unit/logout
+        response.sendRedirect("http://10.40.4.7:8080/EssSealPlatform/unit/logout");
         return false;
     }
 }
--- src/main/java/com/clt/ess/dao/ICertificateDao.java
+++ src/main/java/com/clt/ess/dao/ICertificateDao.java
@@ -5,16 +5,49 @@ import com.clt.ess.entity.Certificate;
 import java.util.List;
 
 public interface ICertificateDao {
-
+    /**
+     * 添加证书记录
+     * @param certificate 证书对象
+     * @return 影响行数
+     */
     int addCertificate(Certificate certificate);
+
+    /**
+     * 更新证书记录
+     * @param certificate 证书对象
+     * @return 影响行数
+     */
     int updateCertificate(Certificate certificate);
+
+    /**
+     * 删除证书记录
+     * @param certificate 证书对象
+     * @return 影响行数
+     */
     int deleteCertificate(Certificate certificate);
+
     Certificate selectCertificate(Certificate certificate);
+
     List<Certificate> selectCertificateList(Certificate certificate);
 
+    /**
+     * 查找证书列表
+     * @param certificate 证书对象
+     * @return 符合条件的证书列表
+     */
     List<Certificate> findCertificate(Certificate certificate);
 
+    /**
+     * 根据证书id查找证书对象
+     * @param certId 证书id
+     * @return 证书对象
+     */
     Certificate findCertificateById(String certId);
 
+    /**
+     * 根据证书hash查找证书对象
+     * @param certHash 证书hash
+     * @return 证书对象
+     */
     Certificate findCertificateByCertHash(String certHash);
 }
